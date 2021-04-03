5c15d6fe736de187572059e58b68a8dca20b807c
goeld9911 <goeld9911@gmail.com>
2021-04-03 15:48:44 +0530

    X Y coordinate changes

--- main.py
+++ main.py
@@ -6,9 +6,27 @@ pygame.init()
 #create the screen
 screen = pygame.display.set_mode((800, 600))
 
-running = True
+#Title and Icon
+pygame.display.set_caption("Coin Invaders")
+icon = pygame.image.load("money.png")
+pygame.display.set_icon(icon)
 
+#Player
+playerImg = pygame.image.load('big.png')
+playerX = 370
+playerY = 480
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+#Game Loop
+running = True
 while running:
+    #RGB --> Red, Green, Blue
+    screen.fill((0, 0 , 0));
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running= False
+    
+    player()
+    pygame.display.update() #Updating the screen color
